5bc20147b2b08b5e178bdf5c6cf112c2500d4120
Yi Lian Liu <z5118177@ad.unsw.edu.au>
2019-04-10 12:37:13 +1000

    Populated fitness database with sample values for categories + their pks

--- Assignment 1/src/assignment/pkg1/Assignment1.java
+++ Assignment 1/src/assignment/pkg1/Assignment1.java
@@ -22,13 +22,15 @@ import javafx.stage.Stage;
  * @author aiisonsuherly
  */
 public class Assignment1 extends Application {
-    
+
     @Override
     public void start(Stage stage) throws Exception {
+        connect();
+        insertCategories();
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
+
         Scene scene = new Scene(root);
-        
+
         stage.setScene(scene);
         stage.show();
     }
@@ -38,51 +40,90 @@ public class Assignment1 extends Application {
      */
     public static void main(String[] args) {
         launch(args);
-        try {
-            connect();
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
-    
-    public static void connect() throws SQLException{
-		//write the four lines which connect your program to the database
+
+    public static void connect() throws SQLException {
+        //write the four lines which connect your program to the database
         Connection conn = DriverManager.getConnection("jdbc:sqlite:fitnessDatabase.db");
         Statement st = conn.createStatement();
 
-        
         String createQuery = "CREATE TABLE IF NOT EXISTS Stats "
                 + "(ID INTEGER PRIMARY KEY autoincrement, "
                 + "CATEGORY TEXT NOT NULL, "
                 + "GOAL INTEGER NULL "
+                + ");";
 
-                +");";
-        
-                st.execute(createQuery);  
-                
+        st.execute(createQuery);
+
         createQuery = "CREATE TABLE IF NOT EXISTS StatsEntries "
                 + "(ID INTEGER PRIMARY KEY autoincrement, "
                 + "STATS_ID INTEGER NOT NULL, "
                 + "VALUE INTEGER NOT NULL, "
                 + "DATE TEXT NULL "
+                + ");";
 
-                +");";
-        
-                st.execute(createQuery);  
+        st.execute(createQuery);
 
-            
         st.close();
         conn.close();
-	}
-	
-	//In javafx class (view controller)
-	//private void OnSaveClicked() {
-            //StatEntry newEntry = new StatEntry( type, volume );
-            
-            //DatabaseHelper.addNewStatEntry(newEntry);
-        }
-        
-       
+    }
     
-   
+ public static void insertCategories() throws SQLException{
+		//write the SQL query and the java code to insert all categories
+        Connection conn = DriverManager.getConnection("jdbc:sqlite:fitnessDatabase.db");
+        Statement st = conn.createStatement();
+        
+        String insertLastHealthCheck = "INSERT OR IGNORE INTO Stats (ID, CATEGORY) "
+                + "VALUES (1, 'Last Health Check');";
+        st.execute(insertLastHealthCheck);
+        
+        String insertLeanFatRatio = "INSERT OR IGNORE INTO Stats (ID, CATEGORY) "
+                + "VALUES (2, 'Lean/Fat Mass Ratio');";
+        st.execute(insertLeanFatRatio);
+        
+        String insertBMI = "INSERT OR IGNORE INTO Stats (ID, CATEGORY) "
+                + "VALUES (3, 'BMI');";
+        st.execute(insertBMI);
+        
+        String insertWellbeing = "INSERT OR IGNORE INTO Stats (ID, CATEGORY) "
+                + "VALUES (4, 'Overall Wellbeing');";
+        st.execute(insertWellbeing);
+
+        String insertSleepRating = "INSERT OR IGNORE INTO Stats (ID, CATEGORY) "
+                + "VALUES (5, 'Sleep Rating');";
+        st.execute(insertSleepRating);
+        
+        String insertRestingHeartRate = "INSERT OR IGNORE INTO Stats (ID, CATEGORY) "
+                + "VALUES (6, 'Resting Heart Rate');";
+        st.execute(insertRestingHeartRate);
+        
+        String insertCaloriesConsumed = "INSERT OR IGNORE INTO Stats (ID, CATEGORY) "
+                + "VALUES (7, 'Calories Consumed');";
+        st.execute(insertCaloriesConsumed);
+        
+        String insertGymAttendance = "INSERT OR IGNORE INTO Stats (ID, CATEGORY) "
+                + "VALUES (8, 'Gym Attendance');";
+        st.execute(insertGymAttendance);
+        
+        String insertStepCount = "INSERT OR IGNORE INTO Stats (ID, CATEGORY) "
+                + "VALUES (9, 'Step Count');";
+        st.execute(insertStepCount);
+        
+        String insertFlights = "INSERT OR IGNORE INTO Stats (ID, CATEGORY) "
+                + "VALUES (10, 'Flights (Stairs) Climbed');";
+        st.execute(insertFlights);
+        
+        String insertResistanceMass = "INSERT OR IGNORE INTO Stats (ID, CATEGORY) "
+                + "VALUES (11, 'Resistance Exercise Mass');";
+        st.execute(insertResistanceMass);
+        
+        
+        st.close();
+        conn.close();   
+
+    //In javafx class (view controller)
+    //private void OnSaveClicked() {
+    //StatEntry newEntry = new StatEntry( type, volume );
+    //DatabaseHelper.addNewStatEntry(newEntry);
+}
+}
